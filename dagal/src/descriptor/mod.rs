16bf161806c7ab96b3e93b00dbcba85da9314305
Danny Le <git@dannydoes.graphics>
2024-05-09 20:45:46 -0400

    Expose more functions

--- dagal/src/descriptor/mod.rs
+++ dagal/src/descriptor/mod.rs
@@ -1,6 +1,7 @@
 pub mod descriptor_set_layout;
 pub use descriptor_set_layout::DescriptorSetLayout;
 pub mod descriptor_layout_builder;
-mod descriptor_pool;
-
 pub use descriptor_layout_builder::DescriptorLayoutBuilder;
+pub mod descriptor_pool;
+pub use descriptor_pool::DescriptorPool;
+pub use descriptor_pool::PoolSizeRatio;
