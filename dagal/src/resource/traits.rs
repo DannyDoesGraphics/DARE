9b173810974cde41ee87fb87a2689f869c4425a5
Danny Le <git@dannydoes.graphics>
2024-05-12 11:11:24 -0400

    Improve resource traits

--- dagal/src/resource/traits.rs
+++ dagal/src/resource/traits.rs
@@ -9,6 +9,12 @@ pub trait Resource<'a>: Destructible + Debug + Sized {
     /// Attempt to create a new resource given the [`Self::CreateInfo`] struct
     fn new(create_info: Self::CreateInfo) -> Result<Self>;
 
+    /// Get a reference to the underlying VkObject.
+    fn get_handle(&self) -> &Self::CreateInfo;
+
+    /// Get a copy to the underlying VkObject
+    fn handle(&self) -> Self::CreateInfo;
+
     /// Get the name of the resource
     fn get_name() -> &'a str;
 }
