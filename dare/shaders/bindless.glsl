c2cf1890378cbc9ffbf5910826c7d0c208e1651b
Danny Le <166263009+DannyDoesGraphics@users.noreply.github.com>
2024-07-05 18:16:49 -0400

    Removed glsl

--- dare/shaders/bindless.glsl
+++
@@ -1,9 +0,0 @@
-#include "surface.glsl"
-#include <dagal/ext.glsl>
-
-layout(set = 0, binding = 0) uniform sampler samplers[];
-layout(set = 0, binding = 1) uniform texture2D sampled_images[];
-layout(set = 0, binding = 2, r32f) uniform image2D storage_images[];
-layout(set = 0, binding = 3) readonly buffer BDA {
-    uint64_t buffer_addresses[];
-};
--- dare/shaders/scene_data.glsl
+++
@@ -1,7 +0,0 @@
-#include <dagal/ext.glsl>
-
-layout (buffer_reference, scalar) readonly buffer SceneData {
-    mat4 view;
-    mat4 proj;
-    mat4 view_proj;
-};
