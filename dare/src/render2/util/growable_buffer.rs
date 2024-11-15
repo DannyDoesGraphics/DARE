8683a46df5c2bad1e88719ea7f83110c3f6d6bd4
DannyDoesGraphics <git@dannydoes.graphics>
2024-11-15 04:06:18 -0500

    Added scaffolding for a growable buffer

---
+++ dare/src/render2/util/growable_buffer.rs
@@ -0,0 +1,37 @@
+use dagal::allocators::Allocator;
+use dagal::ash::vk;
+use std::ops::Deref;
+
+/// blocking changes i need to make:
+/// TODO:
+/// - port over [`vk::DeviceCreateInfo`] into our own custom struct to get rid of the lifetime
+/// requirements
+/// Describes a buffer which can grow dynamically, but shrinks rarely
+pub struct GrowableBuffer<A: Allocator> {
+    handle: std::cell::Cell<dagal::resource::Buffer<A>>,
+}
+impl<A: Allocator> Deref for GrowableBuffer<A> {
+    type Target = dagal::resource::Buffer<A>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.handle
+    }
+}
+
+impl<A: Allocator> GrowableBuffer<A> {
+    pub fn new(handle: dagal::resource::Buffer<A>) -> Self {
+        Self {
+            handle: std::cell::Cell::new(handle),
+        }
+    }
+
+    /// Grows the current buffer by [`dl`]
+    pub fn grow(&mut self, dl: vk::DeviceSize) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    /// Shrinks the current by [`dl`] and effectively cuts off the last [`dl`] bytes
+    pub fn shrink(&mut self, dl: vk::DeviceSize) -> anyhow::Result<()> {
+        todo!()
+    }
+}
--- dare/src/render2/util/mod.rs
+++ dare/src/render2/util/mod.rs
@@ -1,4 +1,6 @@
 pub mod format;
 pub mod gpu_resource_table;
+mod growable_buffer;
 pub mod transfer;
+
 pub use format::*;
