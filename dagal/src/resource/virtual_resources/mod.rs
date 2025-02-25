26330c3898d12f1e7dc86da7b26f55bb021e7c69
DannyDoesGraphics <git@dannydoes.graphics>
2025-02-25 07:04:59 -0500

    Refactor virtual resource and deferred deletion handling

--- dagal/src/resource/virtual_resources/mod.rs
+++ dagal/src/resource/virtual_resources/mod.rs
@@ -7,111 +7,47 @@ use std::marker::PhantomData;
 use std::ops::Deref;
 
 /// Represents a virtual_resources resource
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone)]
 pub struct VirtualResource {
     pub uid: u64,
     pub gen: u64,
+    pub drop_send: Option<crossbeam_channel::Sender<VirtualResource>>,
     pub type_id: TypeId,
 }
-impl AsRef<VirtualResource> for VirtualResource {
-    fn as_ref(&self) -> &VirtualResource {
-        self
+impl PartialEq for VirtualResource {
+    fn eq(&self, other: &Self) -> bool {
+        self.uid == other.uid && self.gen == other.gen && self.type_id == other.type_id
     }
 }
-impl<R: Resource + 'static> PartialEq<VirtualResourceTyped<R>> for VirtualResource {
-    fn eq(&self, other: &VirtualResourceTyped<R>) -> bool {
-        self.uid == other.uid && TypeId::of::<R>() == self.type_id
-    }
-}
+impl Eq for VirtualResource {}
 impl Hash for VirtualResource {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.uid.hash(state);
+        self.gen.hash(state);
         self.type_id.hash(state);
     }
 }
-
-/// Represents a virtual_resources resource handle that is strongly typed
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct VirtualResourceTyped<R: Resource> {
-    pub uid: u64,
-    pub gen: u64,
-    pub _phantom: PhantomData<R>,
-}
-impl<R: Resource + 'static> Hash for VirtualResourceTyped<R> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.uid.hash(state);
-        TypeId::of::<R>().hash(state);
-    }
-}
-impl<R: Resource + 'static> VirtualResourceTyped<R> {
-    /// Attempts to turn a [`VirtualResource`] into a strongly typed [`VirtualResourceTyped`]
-    pub fn from(res: VirtualResource) -> Option<Self> {
-        if TypeId::of::<R>() == res.type_id {
-            Some(Self {
-                uid: res.uid,
-                gen: res.gen,
-                _phantom: PhantomData::default(),
-            })
-        } else {
-            None
+impl Drop for VirtualResource {
+    fn drop(&mut self) {
+        if let Some(drop_send) = &self.drop_send {
+            drop_send.send(Self {
+                uid: self.uid,
+                gen: self.gen,
+                drop_send: None,
+                type_id: self.type_id,
+            }).unwrap();
         }
     }
 }
-impl<R: Resource + 'static> Into<VirtualResource> for VirtualResourceTyped<R> {
-    fn into(self) -> VirtualResource {
-        VirtualResource {
+impl VirtualResource {
+    /// If a virtual resource will destroy the physical resource upon being, dropped, this allows
+    /// you to downgrade the virtual resource
+    pub fn downgrade(&self) -> Self {
+        Self {
             uid: self.uid,
             gen: self.gen,
-            type_id: TypeId::of::<R>(),
+            drop_send: None,
+            type_id: self.type_id,
         }
-    }
-}
-impl<R: Resource> From<VirtualResource> for VirtualResourceTyped<R> {
-    fn from(value: VirtualResource) -> Self {
-        Self {
-            uid: value.uid,
-            gen: value.gen,
-            _phantom: PhantomData::default(),
-        }
-    }
-}
-impl<R: Resource + 'static> PartialEq<VirtualResource> for VirtualResourceTyped<R> {
-    fn eq(&self, other: &VirtualResource) -> bool {
-        self.uid == other.uid && TypeId::of::<R>() == other.type_id
-    }
-}
-
-/// Indicates an instance of a virtual resource that when dropped, will immediately remove the underlying
-/// resource key it holds
-#[derive(Debug)]
-pub struct VirtualResourceDrop {
-    pub(crate) resource: VirtualResource,
-    pub(crate) send_drop: crossbeam_channel::Sender<VirtualResource>,
-}
-impl AsRef<VirtualResource> for VirtualResourceDrop {
-    fn as_ref(&self) -> &VirtualResource {
-        &self.resource
-    }
-}
-impl Deref for VirtualResourceDrop {
-    type Target = VirtualResource;
-
-    fn deref(&self) -> &Self::Target {
-        &self.resource
-    }
-}
-impl PartialEq for VirtualResourceDrop {
-    fn eq(&self, other: &Self) -> bool {
-        self.type_id == other.type_id
-    }
-}
-impl PartialEq<VirtualResource> for VirtualResourceDrop {
-    fn eq(&self, other: &VirtualResource) -> bool {
-        *self == *other
-    }
-}
-impl Drop for VirtualResourceDrop {
-    fn drop(&mut self) {
-        self.send_drop.send(self.resource).unwrap();
     }
 }
--- dare/src/render2/physical_resource/mod.rs
+++ dare/src/render2/physical_resource/mod.rs
@@ -1,30 +1,64 @@
 use std::any::TypeId;
 use std::collections::HashMap;
+use std::sync::Arc;
 use dagal::resource::traits::Resource;
 use dare_containers::prelude as containers;
 use crate::asset2::prelude::AssetHandle;
 use crate::asset2::traits::Asset;
 use crate::render2::render_assets::traits::MetaDataRenderAsset;
 
-pub fn slot_to_virtual_handle<T: 'static>(slot: containers::Slot<T>) -> dagal::resource::VirtualResource {
+pub fn slot_to_virtual_handle<T: 'static>(slot: containers::Slot<T>, drop_send: Option<crossbeam_channel::Sender<dagal::resource::VirtualResource>>) -> dagal::resource::VirtualResource {
     dagal::resource::VirtualResource {
         uid: slot.id(),
         gen: slot.generation(),
+        drop_send,
         type_id: TypeId::of::<T>(),
     }
 }
 
+/// Internalize deletion entry
+struct DeletionSlot {
+    lifetime: u32,
+    current: u32,
+    virtual_resource: Option<Arc<dagal::resource::VirtualResource>>,
+}
+impl DeletionSlot {
+    /// Reset current lifetime
+    pub fn reset(&mut self) {
+        self.current = self.lifetime
+    }
+}
+
 /// Storage for physical resources and used to allocate out [`dagal::resource::VirtualResource`]
+///
+/// # Error handling
+/// When loading, if an asset fails to properly load, we will not record that it failed.
 pub struct PhysicalResourceStorage<T: MetaDataRenderAsset> {
     pub slot: containers::SlotMap<Option<T::Loaded>>,
     /// Map asset handles to virtual resource handles
-    pub asset_mapping: HashMap<AssetHandle<T::Asset>, containers::Slot<Option<T::Loaded>>>,
+    pub asset_mapping: HashMap<AssetHandle<T::Asset>, dagal::resource::VirtualResource>,
+    /// for asset loading
+    loaded_send: crossbeam_channel::Sender<(dagal::resource::VirtualResource, T::Loaded)>,
+    loaded_recv: crossbeam_channel::Receiver<(dagal::resource::VirtualResource, T::Loaded)>,
+    /// for handle drops
+    drop_send: crossbeam_channel::Sender<dagal::resource::VirtualResource>,
+    drop_recv: crossbeam_channel::Receiver<dagal::resource::VirtualResource>,
+    /// We can also perform deferred deletion strategies
+    /// (current_lifetime, old_lifetime, reference)
+    deferred_deletion: HashMap<dagal::resource::VirtualResource, DeletionSlot>,
 }
 impl<T: MetaDataRenderAsset> Default for PhysicalResourceStorage<T> {
     fn default() -> Self {
+        let (loaded_send, loaded_recv) = crossbeam_channel::unbounded();
+        let (drop_send, drop_recv) = crossbeam_channel::unbounded();
         Self {
             slot: Default::default(),
             asset_mapping: Default::default(),
+            loaded_send,
+            loaded_recv,
+            drop_send,
+            drop_recv,
+            deferred_deletion: Default::default(),
         }
     }
 }
@@ -33,8 +67,12 @@ impl<T: MetaDataRenderAsset> PhysicalResourceStorage<T> {
     /// Allocate a virtual resource handle with no physical resource backing
     pub fn get_virtual_handle(&mut self) -> dagal::resource::VirtualResource {
         let slot = self.slot.insert(None);
-        let virtual_resource = slot_to_virtual_handle(slot);
-        virtual_resource
+        slot_to_virtual_handle(slot, None)
+    }
+
+    pub fn get_deferred_virtual_handle(&mut self) -> Arc<dagal::resource::VirtualResource> {
+        let slot = self.slot.insert(None);
+        Arc::new(slot_to_virtual_handle(slot, Some(self.drop_send.clone())))
     }
 
     /// Insert a physical resource to back a virtual resource
@@ -44,17 +82,84 @@ impl<T: MetaDataRenderAsset> PhysicalResourceStorage<T> {
         }).flatten()
     }
 
+    /// Alias an asset handle to a virtual resource
+    pub fn asset_alias(&mut self, virtual_resource: &dagal::resource::VirtualResource, handle: AssetHandle<T::Asset>) {
+        self.asset_mapping.insert(handle, virtual_resource.downgrade());
+    }
+
+    /// Insert a deferred resource
+    pub fn alias_deferred(&mut self, virtual_resource: dagal::resource::VirtualResource, physical_resource: T::Loaded) -> Option<T::Loaded> {
+        self.slot.get_mut(containers::Slot::new(virtual_resource.uid, virtual_resource.gen)).map(|option| {
+            // reset lifetime
+            self.deferred_deletion.get_mut(&virtual_resource).map(|deferred| deferred.reset());
+            option.replace(physical_resource)
+        }).flatten()
+    }
+
     /// Insert a physical resource to back a new virtual resource
     pub fn insert_physical(&mut self, physical_resource: T::Loaded) -> dagal::resource::VirtualResource {
         let virt = self.get_virtual_handle();
-        self.alias(virt, physical_resource);
+        self.alias(virt.clone(), physical_resource);
         virt
     }
 
+    /// Insert a deferred physical resource back to a new virtual resource
+    pub fn insert_deferred_physical(&mut self, lifetime: u32, physical_resource: T::Loaded) -> Arc<dagal::resource::VirtualResource> {
+        let virtual_handle = self.get_deferred_virtual_handle();
+        if lifetime > 0 {
+            let mut deletion = self.deferred_deletion.entry(virtual_handle.downgrade()).or_insert(DeletionSlot {
+                lifetime,
+                current: lifetime,
+                virtual_resource: Some(virtual_handle.clone()),
+            });
+            deletion.reset();
+            deletion.virtual_resource.replace(virtual_handle.clone());
+        }
+        self.alias(virtual_handle.downgrade(), physical_resource); // we don't need to do deferred for now...
+        virtual_handle
+    }
+
+    /// Acquire a channel to notify physical resource storage of successful asset loading
+    pub fn asset_loaded_queue(&self) -> crossbeam_channel::Sender<(dagal::resource::VirtualResource, T::Loaded)> {
+        self.loaded_send.clone()
+    }
+
     /// Attempt to resolve a virtual resource
-    pub fn resolve(&self, virtual_resource: dagal::resource::VirtualResource) -> Option<&T::Loaded> {
+    pub fn resolve(&mut self, virtual_resource: dagal::resource::VirtualResource) -> Option<&T::Loaded> {
         self.slot.get(containers::Slot::new(virtual_resource.uid, virtual_resource.gen))
-            .map(|option| option.as_ref())
+            .map(|option| match option.as_ref() {
+                None => None,
+                Some(r) => {
+                    self.deferred_deletion.get_mut(&virtual_resource).map(|deferred| deferred.reset());
+                    Some(r)
+                },
+            })
             .flatten()
+    }
+
+    /// Process loaded assets
+    pub fn update(&mut self) {
+        // handle inserts
+
+        // decrement lifetimes
+        for deferred in self.deferred_deletion.values_mut().filter(|v| v.lifetime != 0) {
+            let _ = deferred.current.saturating_sub(1);
+            if deferred.current == 0 {
+                // lifetime has reached zero, get rid of it
+                deferred.virtual_resource.take();
+            }
+        }
+        while let Ok((virtual_resource, asset)) = self.loaded_recv.recv() {
+            // find each loaded, update virtual resource to reflect new asset
+            self.slot.get_mut(containers::Slot::new(
+                virtual_resource.uid,
+                virtual_resource.gen,
+            )).unwrap().replace(asset);
+        }
+        // remove dropped
+        while let Ok(virtual_resource) = self.drop_recv.recv() {
+            // drop the physical resource
+            self.slot.get_mut(containers::Slot::new(virtual_resource.uid, virtual_resource.gen)).unwrap().take();
+        }
     }
 }
