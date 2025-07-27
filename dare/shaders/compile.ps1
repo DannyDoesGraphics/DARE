291a668be29c26f7f5ce44032218256ca1448eb8
DannyDoesGraphics <git@dannydoes.graphics>
2025-07-27 12:02:28 -0400

    Added basic culling

--- dare/shaders/compile.ps1
+++ dare/shaders/compile.ps1
@@ -1,2 +1,4 @@
 slangc solid.slang -profile glsl_460 -target spirv -force-glsl-scalar-layout -capability GL_EXT_buffer_reference -emit-spirv-directly -entry vertex_main -o ./compiled/solid.vert.spv
 slangc solid.slang -profile glsl_460 -target spirv -force-glsl-scalar-layout -capability GL_EXT_buffer_reference -emit-spirv-directly -entry fragment_main -o ./compiled/solid.frag.spv
+
+slangc cull.slang -profile glsl_460 -target spirv -force-glsl-scalar-layout -capability GL_EXT_buffer_reference -emit-spirv-directly -entry main -o ./compiled/cull.comp.spv
